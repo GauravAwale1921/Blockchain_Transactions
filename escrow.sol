f8b5f4ab67d945c801c88b80b823e526ac429d7d
Gaurav Awale <78631222+GauravAwale1921@users.noreply.github.com>
2023-06-07 16:51:48 +0530

    Add files via upload

---
+++ escrow.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract Escrow {
+    address public sender;
+    address payable public receiver;
+    address public constant approver = 0xabeD470Cf6aebce5530A363E0F5f8334A9A252f4;
+
+
+    function deposit(address payable _receiver) external payable {
+        require(msg.value > 0);
+        sender = msg.sender;
+        receiver = _receiver;
+    }
+
+    function viewApprover() external pure returns(address) {
+        return(approver);
+    }
+
+    function approve() external {
+        require(msg.sender == approver);
+        receiver.transfer(address(this).balance);
+    }
+}
